1b08e959d0a0f84b6687b601ba4c7b56fbbe62c3
Usuario-s <sajer032@gmail.com>
2026-08-20 13:16:34 -0500

    Add header and image display to app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ from PIL import Image
 st.title("y un sabio dijo")
 
 
-
+st.header("en este jdklajkl")
+st.write("slkjflksjlk entonces dijeron")
+image = Image.open("tin.JPG")
+st.image(image, caption="interfaces multimodales")
