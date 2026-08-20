5f0ab676a6337cd658a1fd5a1224db36bfe31e28
Usuario-s <sajer032@gmail.com>
2026-08-20 13:20:55 -0500

    Fix image filename case sensitivity

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ st.title("y un sabio dijo")
 
 st.header("en este jdklajkl")
 st.write("slkjflksjlk entonces dijeron")
-image = Image.open("tin.JPG")
+image = Image.open("tin.jpg")
 st.image(image, caption="interfaces multimodales")
